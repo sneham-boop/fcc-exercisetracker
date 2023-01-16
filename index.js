66759e54a60791b8e756f39eec60216b997c74cd
Sneha Mahajan <sneh.km@gmail.com>
2023-01-16 15:19:47 -0500

    Add mongoose and nodemon.

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+const bodyParser = require('body-parser');
+
+// Database connection
+const mongoose = require('mongoose');
+mongoose
+  .connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(()=>console.log('Connected to database.'))
+  .catch(e=>console.log("No connection to database.",e));
+
+const Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  username: { type: String, required: true },
+  count: Number,
+ log: [{
+      description: String,
+  duration: Number,
+  date: Date
+ }]
+});
+let User = mongoose.model('User', userSchema);
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
